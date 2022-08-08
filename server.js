af9b6538df284490c513778bab9f2cd012b7f594
Alex Standfield <AlexStandfield@gmail.com>
2022-08-08 15:22:29 -0600

    First Lesson

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals.json');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -41,15 +42,28 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id[0]);
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
-
 });
 
-app.listen(3001, () => {
-    console.log(`API server is now on port 3001`);
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API server is now on port ${PORT}!`);
 });
